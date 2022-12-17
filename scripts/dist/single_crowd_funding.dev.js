14421e5a7b33b2902008d5f8ae8ad9265f9abb66
Odiase <odiaseefosa18@gmail.com>
2022-12-17 13:46:15 +0100

    added javascript for delete animation and functionality

--- scripts/dist/single_crowd_funding.dev.js
+++ scripts/dist/single_crowd_funding.dev.js
@@ -4,7 +4,11 @@
 var show_fundings_btn = document.querySelector(".show_other_fundings");
 var hide_fundings_btn = document.querySelector(".hide_other_fundings"); // crowd_funding_container for mobile
 
-var mobile_crowd_funding_container = document.querySelector(".crowd_fundings"); // event listeners and actions
+var mobile_crowd_funding_container = document.querySelector(".crowd_fundings"); // delete form variables
+
+var delete_btn = document.querySelector(".delete_btn");
+var delete_form = document.querySelector(".delete_form");
+var cancel_delete_btn = document.querySelector(".cancel-btn"); // event listeners and actions
 
 show_fundings_btn.addEventListener("click", function () {
   mobile_crowd_funding_container.style.overflow = "visible";
@@ -17,4 +21,11 @@ hide_fundings_btn.addEventListener("click", function () {
   mobile_crowd_funding_container.style.height = "0";
   hide_fundings_btn.style.display = "none";
   show_fundings_btn.style.display = "block";
+}); // delete form event listeners
+
+cancel_delete_btn.addEventListener("click", function () {
+  delete_form.style.top = "-100%";
+});
+delete_btn.addEventListener("click", function () {
+  delete_form.style.top = "0";
 });
--- scripts/single_crowd_funding.js
+++ scripts/single_crowd_funding.js
@@ -5,6 +5,11 @@ var hide_fundings_btn = document.querySelector(".hide_other_fundings");
 // crowd_funding_container for mobile
 var mobile_crowd_funding_container = document.querySelector(".crowd_fundings");
 
+// delete form variables
+var delete_btn = document.querySelector(".delete_btn");
+var delete_form = document.querySelector(".delete_form");
+var cancel_delete_btn = document.querySelector(".cancel-btn");
+
 
 // event listeners and actions
 show_fundings_btn.addEventListener("click", () => {
@@ -19,4 +24,13 @@ hide_fundings_btn.addEventListener("click", () => {
     mobile_crowd_funding_container.style.height = "0";
     hide_fundings_btn.style.display = "none";
     show_fundings_btn.style.display = "block";
+})
+
+
+// delete form event listeners
+cancel_delete_btn.addEventListener("click", () => {
+    delete_form.style.top = "-100%";
+})
+delete_btn.addEventListener("click", () => {
+    delete_form.style.top = "0";
 })
